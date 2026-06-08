501a388437702a1dec4c5e514902c348f1b65a02
Ali sirjanii <alisi373737@gmail.com>
2026-06-08 15:28:11 +0330

    new update

--- src/models/RefreshToken.js
+++ src/models/RefreshToken.js
@@ -1,31 +1,30 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 
-const refreshTokenSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    index: true,
-  },
+const refreshTokenSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+            index: true,
+        },
 
-  tokenHash: {
-    type: String,
-    required: true,
-    index: true,
-  },
+        tokenHash: {
+            type: String,
+            required: true,
+            index: true,
+        },
 
-  expiresAt: {
-    type: Date,
-    required: true,
-    index: { expires: 0 },
-  },
-}, {
-  timestamps: true,
-});
-
-refreshTokenSchema.statics.hashToken = function (token) {
-  return crypto.createHash('sha256').update(token).digest('hex');
-};
+        expiresAt: {
+            type: Date,
+            required: true,
+            index: { expires: 0 },
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
